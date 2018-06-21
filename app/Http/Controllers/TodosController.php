9360c2e033e9ee9a50122ae06de0b99cbb04b01c
Freddie Molina <freddiejmo@gmail.com>
2018-06-21 17:55:36 -0400

    added session message

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Todo;
+use Session;
 
 class TodosController extends Controller
 {
@@ -22,6 +23,7 @@ class TodosController extends Controller
         $todo->todo = $request->todo;
         $todo->save();
 
+        Session::flash('success', 'Your todo was created.');
         return redirect()->back();
     }
 
@@ -31,6 +33,7 @@ class TodosController extends Controller
 
         $todo->delete();
 
+        Session::flash('success', 'Your todo was deleted.');
         return redirect()->back();
     }
 
@@ -49,6 +52,7 @@ class TodosController extends Controller
 
         $todo->save();
 
+        Session::flash('success', 'Your todo was updated.');
         return redirect()->route('todos');
     }
 
@@ -59,6 +63,7 @@ class TodosController extends Controller
         $todo->completed = 1;
         $todo->save();
 
+        Session::flash('success', 'Your todo was marked as completed.');
         return redirect()->back();
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -68,6 +68,11 @@
         </style>
     </head>
     <body>
+      @if(Session::has('success'))
+        <div class="alert alert-success" role="alert">{{ Session::get('success') }}...</div>
+      @endif
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <br><br>
         <div class="flex-center position-ref full-height">
 
             <div class="content">
